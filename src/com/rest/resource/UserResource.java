f000dcc6fb30bc7257f41d2b50487c45b69f0f22
sunwenhui <1095494616@qq.com>
2018-06-06 14:46:12 +0800

    分支

--- src/com/rest/resource/UserResource.java
+++ src/com/rest/resource/UserResource.java
@@ -2,4 +2,12 @@ package com.rest.resource;
 
 public class UserResource {
 
+	
+	public static void main(String[] args) {
+		System.out.println("这提交到分支");
+	}
+	
+	public void demo(){
+		System.out.println("这也是");
+	}
 }
