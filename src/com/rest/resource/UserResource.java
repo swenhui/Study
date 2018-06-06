98001b93399d02625248feec9a9a2a5a37869976
sunwenhui <1095494616@qq.com>
2018-06-06 14:51:26 +0800

    主线提交

--- src/com/rest/resource/UserResource.java
+++ src/com/rest/resource/UserResource.java
@@ -2,4 +2,14 @@ package com.rest.resource;
 
 public class UserResource {
 
+	
+	public static void main(String[] args) {
+		System.out.println("这是提交到分支的啊啊啊啊啊啊啊啊啊啊啊");
+	}
+	
+	public void demo(){
+		System.out.println("这也是分支中的哦哦哦哦哦哦");
+	}
+	
+	
 }
